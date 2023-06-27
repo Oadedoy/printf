577210c22720fa59653837ad0059b84962fdab72
Tosho Joy <omotosojoy@gmail.com>
2023-06-27 11:47:33 +0100

    fixes to task 6

--- pointers.c
+++ pointers.c
@@ -8,26 +8,6 @@ int print_pointer(va_list args)
 {
 	void *ptr = va_arg(args, void *);
 	unsigned long int value = (unsigned long int)ptr;
-	char map_to[] = "0123456789abcdef";
-	int len = 0;
 
-	len += _putchar('0');
-	len += _putchar('X');
-
-	if (value == 0)
-		len += _putchar('0');
-	else
-	{
-		unsigned long int new_value = value;
-
-		while (new_value > 0)
-		{
-			int digit = new_value % 16;
-
-			len += _putchar(map_to[digit]);
-			new_value /= 16;
-			len++;
-		}
-	}
-	return (len);
+	return (print_hex(value));
 }
