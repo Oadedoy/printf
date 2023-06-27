5591ca3ef77b823e24d412efc10d7b3ee66bd9d2
Tosho Joy <omotosojoy@gmail.com>
2023-06-27 12:20:21 +0100

    fixes to Task 6

--- pointers.c
+++ pointers.c
@@ -6,8 +6,7 @@
  */
 int print_pointer(va_list args)
 {
-	void *ptr = va_arg(args, void *);
-	unsigned long int value = (unsigned long int)ptr;
+	char *ptr = va_arg(args, void *);
 
-	return (print_hex(ptr));
+	return (print_hex(args));
 }
